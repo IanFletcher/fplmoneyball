6465e0493e50656902da83f9d70254efcb6e65c4
IanFletcher <ianfletcher9@gmail.com>
2013-10-09 21:12:42 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fplmoneyball::Application.config.secret_key_base = '499a6a5ac5105d9405672375beacd3d94ce6656760fe96d1d6ded27ed9a928d4ab97707e9a768d76d4dc6acdd62ca471dd29684e930c9b3be2d9b648469dfd4c'
