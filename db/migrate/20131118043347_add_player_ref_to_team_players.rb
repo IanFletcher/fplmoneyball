a69d0d8979b252974d77f6f974b9858f07f8605c
Ian Fletcher <ianfletcher9@gmail.com>
2014-04-04 15:05:21 +1100

    fixed migration AddPlayerRef for heroku

--- db/migrate/20131118043347_add_player_ref_to_team_players.rb
+++ db/migrate/20131118043347_add_player_ref_to_team_players.rb
@@ -1,5 +1,7 @@
 class AddPlayerRefToTeamPlayers < ActiveRecord::Migration
   def change
-    add_reference :team_players, :player, :reference
+  #  add_reference :team_players, :player, :reference
+    add_column :team_players, :player_id, :integer
+    add_index :team_players, :player_id
   end
 end
